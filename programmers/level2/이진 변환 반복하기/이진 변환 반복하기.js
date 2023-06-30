21538fc9f0124aa52d059478ed008e26ea3a7063
sonbaejun <sonqowns2@naver.com>
2023-06-30 15:15:20 +0900

    docs : 주석 추가

--- programmers/level2/이진 변환 반복하기/이진 변환 반복하기.js
+++ programmers/level2/이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,18 +1,18 @@
 function solution(s) {
     let spliceZero = 0;
     let cnt = 0;
-    while (1) {
+    while (s.length > 1) {
         cnt++;
         let arr = s.split('');
         let arrLength = arr.length;
+        /* 0제거 */
         for (let i = arrLength - 1; i >= 0; i--) {
             if (arr[i] == 0) {
                 arr.splice(i, 1);
             }
         }
         spliceZero += arrLength - arr.length;
-        s = arr.length.toString(2);
-        if (s == "1") break;
+        s = arr.length.toString(2); // 2진수 변환
+        if (s == "1") return [cnt, spliceZero]; // return value
     }
-    return [cnt, spliceZero];
 }
